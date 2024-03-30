df9305efe825b2bcf016703e02f0547cfa3e91a5
pooja-404 <pk3924792@gmail.com>
2024-03-30 11:08:09 +0530

    commit

--- src/components/Gamesec.jsx
+++ src/components/Gamesec.jsx
@@ -32,14 +32,14 @@ const Gamesec = () => {
         <div className='pt-4 pb-lg-5 position-relative z-1 overflow-x-clip' id='about'>
             <div className="container pt-lg-5 pb-3">
                 <div className="row py-lg-5">
-                    <div className="col-lg-7" data-aos="fade-right">
+                    <div className="col-lg-6 " data-aos="fade-right">
                         <div className='btn_center'>
                             <p className='font-roboto fs-s lh-19 fw-normal graident-btnn mb-0  color-gradient box '>DOERS OVER TALKERS</p></div>
                         <h2 className='font-roboto fs-xl lh-62 fw-bold text-white pt-4  text_c_center'>Game  <span className='d-lg-block'>First.Always.</span></h2>
                     </div>
-                    <div className="col-lg-5 " data-aos="fade-left">
-                        <h2 className='font-roboto fs-sm lh-21 fw-medium text-white pt-lg-5 text_c_center'>This is difficult</h2>
-                        <p className='font-roboto fs-s lh-24 fw-normal off-gray mb-0 text_c_center '>Web3 Games have a notoriously high barrier-to-entry for casual gamers who make up the vast majority of players. “Link your wallet” before gameplay is not just off-putting, it’s boring. We’re solving that with <span className='fw-bold'>world-class storytelling,</span> world-building, game design, art and sound. And fun.</p>
+                    <div className="col-lg-6" data-aos="fade-left">
+                        <h2 className='font-roboto fs-sm lh-21 fw-medium text-white pt-lg-5 text_c_center '>This is difficult</h2>
+                        <p className='font-roboto fs-s lh-24 fw-normal off-gray mb-0 text_c_center max-w-517 '>Web3 Games have a notoriously high barrier-to-entry for casual gamers who make up the vast majority of players. “Link your wallet” before gameplay is not just off-putting, it’s boring. We’re solving that with <span className='fw-bold'>world-class storytelling,</span> world-building, game design, art and sound. And fun.</p>
                     </div>
                 </div>
             </div>
